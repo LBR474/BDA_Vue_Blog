8a308d9a46d48c7b40bac75a6e56cd447992733d
Fake Name <fakename@fake.com>
2024-03-28 15:09:48 +1100

    Add extra picture

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@ import HelloWorld from './components/HelloWorld.vue'
     </a> <br>
     <img src="/Milan_San_Remo_Main_Image.png" class="logo" alt="Vite logo" />
     <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+      <img src="./assets/sunda_distance.png" class="logo vue" alt="Vue logo" />
     </a>
   </div>
   <HelloWorld msg="Vite + Vue" />
